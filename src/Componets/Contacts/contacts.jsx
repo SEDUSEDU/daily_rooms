1fb20da8be223c5b9c93cdfe344a36839d547439
Dubrovskaya Oksana <oxanadu@yandex.ru>
2020-11-10 17:37:08 +0300

    form2

--- src/Componets/Contacts/contacts.jsx
+++ src/Componets/Contacts/contacts.jsx
@@ -12,13 +12,21 @@ const Contacts = (props) => {
                 <img className={c.section_big_img} src="images/big_contacts.jpg" alt="img"/>
             <h3>Контакты</h3>
             </section>
-            <Form />
+            {/*<Form />*/}
             <section className={c.section_mini}>
                 <img className={c.section_mini_img} src="images/mini_contacts.jpg" alt="img"/>
             </section>
-            <div className={c.chat}>
-                <div className={c.title}>Отправьте нам сообщение</div>
-                <textarea type="text" className="chat_message" id=""></textarea>
+            <div className={c.box_chat}>
+                <div className={c.box_chat_top}>
+                    <div className={c.title}>Отправьте нам сообщение</div>
+                    <button className={c.btn_close}>x</button>
+                </div>
+                <div className={c.box_chat_middle}></div>
+                <div className={c.box_chat_bottom}>
+                    <textarea type="text" className={c.chat_message} placeholder="Введите сообщение..."></textarea>
+                    
+                </div>             
+                
 
             </div>
             <Footer />
--- src/Componets/Nav/nav.jsx
+++ src/Componets/Nav/nav.jsx
@@ -7,6 +7,10 @@ const Nav = (props) => {
         <div className={c.nav}>
             <div className={c.nav_menu}>
             <img className={c.logo_menu} src="./images/logo.png" alt="логотип"/>
+            <div className={c.nav_adnin}>
+                <div className={c.admin}>A</div>
+                <div className={c.user}>U</div>
+            </div>
             <div className={c.bar_menu}>
                 <div className={c.line}></div>
                 <div className={c.line}></div>
