e2922b298fffa77d794c282161f660317289e6f5
SergeyDubrovski <sovelectron@rambler.ru>
2020-11-11 18:01:07 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Contacts from './Componets/Contacts/contacts';
 
 const App = (props) => {
+
   return (
-    <Contacts />
-    
-    
+    <BrowserRouter>
+      <Contacts />
+    </BrowserRouter>
   );
 }
 
--- src/Componets/Nav/nav.jsx
+++ src/Componets/Nav/nav.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import c from './nav.module.css';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import c from "./nav.module.css";
 
 const Nav = (props) => {
-    return (
-        <div className={c.nav}>
-            <div className={c.nav_menu}>
-            <img className={c.logo_menu} src="./images/logo.png" alt="логотип"/>
-            <div className={c.nav_adnin}>
-                <div className={c.admin}>A</div>
-                <div className={c.user}>U</div>
-            </div>
-            <div className={c.bar_menu}>
-                <div className={c.line}></div>
-                <div className={c.line}></div>
-                <div className={c.line}></div>
-            </div>
-            </div>
+  return (
+    <div className={c.nav}>
+      <div className={c.nav_menu}>
+        <img className={c.logo_menu} src="./images/logo.png" alt="логотип" />
+        <div className={c.nav_adnin}>
+          <NavLink to="/" className={c.admin}>
+            GO
+          </NavLink>
+          <NavLink to="/admin" className={c.admin}>
+            A
+          </NavLink>
+          <NavLink to="/user" className={c.user}>
+            U
+          </NavLink>
         </div>
+        <div className={c.bar_menu}>
+          <div className={c.line}></div>
+          <div className={c.line}></div>
+          <div className={c.line}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-} 
-
-export default Nav
+export default Nav;
