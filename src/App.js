7739b042d2990f94b4549b86facbd9cceb98e71c
Dubrovskaya Oksana <oxanadu@yandex.ru>
2020-11-11 19:00:56 +0300

    form3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = (props) => {
     <BrowserRouter>
       <Contacts />
     </BrowserRouter>
+
   );
 }
 
---
+++ src/Componets/Contacts_user/Contacts_user.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import c from './Contacts_user';
+import Nav from '../Nav/nav'
+import Form from '../Form/form';
+import Footer from '../Footer/footer';
+
+const Contacts_user = (props) => {
+    return (
+        <div className={c.contacts}>
+            <Nav />
+            <section className={c.section_big}>
+                <img className={c.section_big_img} src="images/big_contacts.jpg" alt="img"/>
+            <h3>Контакты</h3>
+            </section>
+            {/*<Form />*/}
+            <section className={c.section_mini}>
+                <img className={c.section_mini_img} src="images/mini_contacts.jpg" alt="img"/>
+            </section>
+            <div className={c.box_chat}>
+                <div className={c.box_chat_top}>
+                    <div className={c.title}>Отправьте нам сообщение</div>
+                    <button className={c.btn_close}>x</button>
+                </div>
+                <div className={c.box_chat_middle}></div>
+                <div className={c.box_chat_bottom}>
+                    <textarea type="text" className={c.chat_message} placeholder="Введите сообщение..."></textarea>
+                    
+                </div>             
+                
+
+            </div>
+            <Footer />
+  
+        </div>
+
+       
+    )
+} 
+
+export default Contacts_user
