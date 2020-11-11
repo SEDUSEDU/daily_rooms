ae19a63e69906eff72c2d586273472454d147dbb
SergeyDubrovski <sovelectron@rambler.ru>
2020-11-11 19:27:25 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Contacts from './Componets/Contacts/contacts';
+import Contacts_admin from './Componets/Contacts_admin/Contacts_admin';
+import Contacts_user from './Componets/Contacts_user/Contacts_user'
 
 const App = (props) => {
 
   return (
     <BrowserRouter>
+    <Route exact path="/">
       <Contacts />
+      </Route> 
+    <Route exact path="/admin" component={Contacts_admin}/>
+          
+      <Route exact path="/user" component={Contacts_user}/>
+     
+       
+      
     </BrowserRouter>
 
   );
